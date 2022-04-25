35572b4cc15f10be2c84299d9825c67b1997d972
Jobson Lira <jobson.lira@gmail.com>
2022-04-25 08:10:53 -0300

    chore: initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://admin:BMvYyq6hl9IMj6Z8@nestjs-mongo.kleiq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    ),
+    UsersModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
